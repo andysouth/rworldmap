fadd892073068a1960ec07547e13b0d2c3ec10e6
southandy <southandy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-20 18:46:03 +0000

    trying to fix this compilation check error which occurs on linux but not windows or mac * checking S3 generic/method consistency ... WARNING Error in get(fname, envir = envir, inherits = FALSE) :   internal error -3 in R_decompress1 Calls:  ... Filter -> sapply -> lapply -> FUN -> .is_S3_generic -> get Execution halted See section ‘Generic functions and methods’ of the ‘Writing R Extensions’ manual.
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/rworldmap/pkg/rworldmap@176 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/getMap.r
+++ R/getMap.r
@@ -14,39 +14,35 @@ function(resolution="coarse",projection=NA){
     warning("resolution should be set to one of :",paste(resolutionOptions,""),"\nsetting to coarse as default\n")
     resolution="coarse"
   }
-  
-  
+    
   
   if (! is.na(projection)) 
      {
       warning("the projection argument to getMap() in rworldmap is deprecated and will be removed in a future release.
 			        Returning an unprojected map, use spTransform() from package rgdal to project.")
      }
-  #
 
   #coarsest resolution map
  if (resolution == "coarse") {
-    #data("wrld_coarse", envir = environment(),package = "rworldmap")
-    #mapWithData <- get("wrld_coarse")
     data("countriesCoarse", envir = environment(),package = "rworldmap")
-    mapWithData <- get("countriesCoarse")    
+    mapWithData <- get("countriesCoarse", envir = environment(), inherits=FALSE) 
+    
   }  else if (resolution == "low") {
-    #data("wrld_simpl", envir = environment(), package = "rworldmap")
-    #mapWithData <- get("wrld_simpl")
     data("countriesLow", envir = environment(),package = "rworldmap")
-    mapWithData <- get("countriesLow")    
+    mapWithData <- get("countriesLow", envir = environment(), inherits=FALSE) 
+    
   }  else if (resolution == "less islands" | resolution == "li") {
-    #data("wrld_simpl", envir = environment(), package = "rworldmap")
-    #mapWithData <- get("wrld_simpl")
     data("countriesCoarseLessIslands", envir = environment(),package = "rworldmap")
-    mapWithData <- get("countriesCoarseLessIslands")    
+    mapWithData <- get("countriesCoarseLessIslands", envir = environment(), inherits=FALSE)   
+    
   }  else if (resolution == "high" && !require(rworldxtra, quietly=TRUE)) {
     warning("for resolution='high' option you need to install package rworldxtra, using low resolution version for now")
-    data("countriesLow", envir = environment(),package = "rworldmap")
-    mapWithData <- get("countriesLow")    
+    data("countriesLow", envir = environment(), package = "rworldmap")
+    mapWithData <- get("countriesLow", envir = environment(), inherits=FALSE)
+    
   }  else if (resolution == "high" ) {
-    data("countriesHigh", envir = environment(),package = "rworldxtra")
-    mapWithData <- get("countriesHigh")    
+    data("countriesHigh", envir = environment(), package = "rworldxtra")
+    mapWithData <- get("countriesHigh", envir = environment(), inherits=FALSE)    
   }  
 
   
